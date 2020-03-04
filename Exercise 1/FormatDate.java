6508332148963bc0dc44f41c4e5d690bd924702e
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-04 12:37:28 +0800

    Delete FormatDate.java

--- Exercise 1/FormatDate.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class FormatDate{
-public static void main (String []args){
-Scanner In = new Scanner (System.in);
-String Date =In.next();
-String newDateFormat = Date.replaceAll("/","."); 
-System.out.println("The final String is : " + newDateFormat);
-}
-} 
