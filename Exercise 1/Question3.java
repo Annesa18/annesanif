e7a470faa443397ffff57b30f88893c85cbcf4dc
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-05 15:47:47 +0800

    Update Question3.java

--- Exercise 1/Question3.java
+++ Exercise 1/Question3.java
@@ -1,15 +1,15 @@
-import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat; 
 import java.util.Date;
-public class Question3{  
-public static void main(String[] args){
-Date date = new Date();  
-String SimpleDateFormat;
-SimpleDateFormat formatter= new SimpleDateFormat("dd/MM/yyyy");
+public class Date{ //to show class Date
+public static void main(String[] args){ //main method
+Date date = new Date();  //to create object
+String SimpleDateFormat; // declaring SimpleDateFormat is String
+SimpleDateFormat formatter= new SimpleDateFormat("dd/MM/yyyy"); 
 String strDate = formatter.format(date);   
 System.out.println(strDate); 
 System.out.println(" ");
 SimpleDateFormat f = new SimpleDateFormat("dd.MM.yyyy");
-System.out.println(f.format(new Date()));
+System.out.println(f.format(new Date())); // display new date 
 }  
 } 
     
