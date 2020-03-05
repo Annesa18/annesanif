38b9373e0b6f9a6d865778241c1f93c27d392c32
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-05 18:21:21 +0800

    Add files via upload

---
+++ Exercise2/CountAexe.java
@@ -0,0 +1,21 @@
+public class CountAexe{
+public static void main (String []args){
+
+String str = "javA";
+int count =0;
+
+for (int i=0; i<str.length();i++){
+    if (str.charAt(i)=='A'){
+    count++;
+    }
+   }
+System.out.println("Count of A is found:" + count);
+}
+}    
+
+
+
+
+
+
+
