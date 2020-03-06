e5aa1828538171192b95e5bd40e50dd91fa678e3
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-06 17:05:08 +0800

    Add files via upload

--- Exercise2/CountAexe.java
+++ Exercise2/CountAexe.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 public class CountAexe{
 public static void main (String []args){
 
-String str = "javA";
+Scanner in = new Scanner (System.in);
+String str;
 int count =0;
+
+System.out.println("Enter the string:");
+str= in.nextLine();
 
 for (int i=0; i<str.length();i++){
     if (str.charAt(i)=='A'){
