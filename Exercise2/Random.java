14568b828c8ee9070df94a5bb88ccf4c6d429f25
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-07 19:31:30 +0800

    Update Random.java

--- Exercise2/Random.java
+++ Exercise2/Random.java
@@ -25,38 +25,6 @@ public class Random{ // to show Random class
          
       //display random integer between first and second number   
       System.out.println("The random integer is:" + count);
-  }
-  }
-         
-      
-   
-      
-
-
-/*import java.util.*;
-public class Random{
-public static void main (String []args){
-
-   Scanner in = new Scanner (System.in);
-   int []first = new int [data];
-   int []second = new int [data];
-   System.out.println("Enter the first number:");
-   first =in.nextInt();
-   System.out.println("Enter the second number:");
-   second =in.nextInt();
-   new RandomInRange(first,second);
-   
   }//end main
-
-public int randomInRange (int[] first, int[] second){
-   int result = 0;
-   random [] generator = new Random();
-      if (first > second)
-   {
-      int range = first - (second + 1);
-      result = generator.nextInt(range) + first;
-   }
-   return result;
- 
-  }
-}//end class*/
+  }//end class
+         
