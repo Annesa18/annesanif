e40078d4bd4e750e27adc4380d82451bb657d611
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-09 15:01:53 +0800

    Add files via upload

---
+++ Exercise3/Random_exe3.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class Random_exe3{
+   public static void main (String []args){
+   
+      Random rd = new Random();
+      System.out.println(rd.nextInt(41) + 10);
+  }
+}
+   
