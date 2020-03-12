b5bf992899664eb4e611027e1b00be0b4fe23a5d
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-12 15:35:26 +0800

    Add files via upload

---
+++ test/Test_2.java
@@ -0,0 +1,20 @@
+public class Test_2{ 
+   
+   static int i;
+   static long l;
+   static float f;
+   static double d;
+   static char ch;
+   static boolean bool;
+   
+   public static void main (String []val){
+   
+   System.out.println(i);
+   System.out.println(l);
+   System.out.println(f);
+   System.out.println(d);
+   System.out.println(ch);
+   System.out.println(bool);
+   }
+ }
+   
