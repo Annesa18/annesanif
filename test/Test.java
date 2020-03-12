c876f7ccafd138495d0ccdcbb2653c4134e29b4e
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-12 15:29:33 +0800

    Add files via upload

---
+++ test/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+   public static void main (String []args){
+   
+   String word = "Hello world!";
+   System.out.println(word);
+   }
+ }
---
+++ test/Test_3.java
@@ -0,0 +1,15 @@
+public class Test_3{
+   public static void main (String []args){
+   
+   String names = "Ahmad";
+   String names1 = "Ahmad";
+   
+   if (names == names1)
+      System.out.println("Equal");
+   else 
+      System.out.println("Not Equal");
+      
+   }
+}
+      
+   
