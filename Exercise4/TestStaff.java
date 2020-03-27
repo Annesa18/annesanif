da741b64d87efe4a045fa8e901b5a9ada4a0876b
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-27 21:17:21 +0800

    Add files via upload

---
+++ Exercise4/TestStaff.java
@@ -0,0 +1,87 @@
+package teststaff;
+/**
+ *
+ * @author nurul annesa
+ */
+import java.util.Scanner;
+public class TestStaff {
+
+    public static void main(String[] args) {
+        
+      String nm,id;
+      int wDay;
+      Scanner console = new Scanner(System.in);
+      Staff s = new Staff ();
+      
+      System.out.println("Enter your Staff name:");
+      nm = console.next();
+      System.out.println("Enter your Staff ID:");
+      id = console.next();
+      System.out.println("Enter your Staff working perDay:");
+      wDay = console.nextInt();
+      
+      //display staff info
+        s.setStaff(nm, id, wDay);
+        s.calculateSalary();
+         System.out.println("---Staff Info---");
+         System.out.println("Staff Name: " + s.getName());
+         System.out.println("Staff ID: " + s.getStaffID());
+         System.out.println("Staff working per day: " + s.getWorkingDay() + "days");
+         System.out.println("Staff salary:" + "RM" + s.getSalary());
+         System.out.println("-----------------");
+   }//end main
+}
+
+ class Staff {
+    private String name;
+    private int  workingDay;
+    private String staffID;
+    private double calculateSalary;
+    private double Salary;
+    private double salary;
+    
+    public void setStaff(String nm,String id,int wDay) {
+        name= nm;
+        staffID = id;
+        workingDay = wDay;
+    }
+    
+    public void setName(String nm){
+        name = nm;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setWorkingDay(int wDay){
+        workingDay = wDay;
+    }
+    
+    public int getWorkingDay() {
+        return workingDay;
+    }
+    
+    public void setStaffID(String id){
+        staffID = id;
+    }
+    
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    
+    public double calculateSalary() {
+        salary = workingDay * 35.00;
+        return salary;
+    }
+
+}
+
+
+      
+      
+      
