d1601cb557892b6958d5179d9e3200dc21f6e275
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-03-31 21:45:49 +0800

    Update TestMyFCStaff.java

--- Exercise4/TestMyFCStaff.java
+++ Exercise4/TestMyFCStaff.java
@@ -3,14 +3,11 @@ package testmyfcstaff;
 import java.util.Scanner;
 public class TestMyFCStaff {
 
-    private static double salary;
-    private static double sales;
-
     public static void main(String[] args) {
         String name,staffID;
         int hourWorks;
-        MyFCStaff fc = new MyFCStaff ();
-        
+        double sales;
+                
         Scanner in = new Scanner (System.in);
         System.out.println("Enter your staff name: ");
         name = in.next();
@@ -20,81 +17,52 @@ public class TestMyFCStaff {
         sales = in.nextDouble();
         System.out.println("Enter your hour works:");
         hourWorks = in.nextInt();
-       
+        
+        //create object
+        MyFCStaff fc = new MyFCStaff (name,staffID,hourWorks,sales);
         //invoked method
-        fc.setStaff(name,staffID,hourWorks,sales,salary);
-        fc.calculateSalary();
-        fc.calculateCommission();
         fc.displaySalary();
     }
 }
 
  class MyFCStaff {
     private String name,staffID;
-    private int totalHourWorks;
-    private double sales,salary,commission,totalSales;
+    private int hourWorks;
+    private double sales,salary,commission,totalSales,totalSalary;
    
      //constructer
-    public void setStaff(String nm,String id,int hWorks,double  sales,double salary){
+    public MyFCStaff(String nm,String id,int hWorks,double  sales){
         name = nm;
         staffID = id;
-        totalHourWorks = hWorks;
+        hourWorks = hWorks;
         this.sales = sales;
-        this.salary = salary;
     }
-    
-    public void setName(String nm){
-        name = nm;
+       
+    public double calculateSalary() {
+        totalSalary = (hourWorks * 8 ) + calculateCommission();
+        return totalSalary;
     }
-    
-    public void setStaffID(String id){
-         staffID = id;
-    }
-    public void setHourWorks(int hWorks){
-        totalHourWorks = hWorks;
-    }
-   
-    public String getName() {
-        return name;
-    }
-    
-    public String getStaffID(){
-        return staffID;
-    }
-    
-    public int getTotalHourWorks() {
-        return totalHourWorks;
-    }
-    
-    public void calculateSalary() {
-        salary = (totalHourWorks * 8 ) + commission;
-    }
-    
-    public double getSalary(){
-        return salary;
-    }
-    
-    public void calculateCommission() {
-        if (sales >= 150 && sales <= 300)
-            commission =(int) sales * 0.05;
-        else if (sales >= 301 && sales <= 500)
-            commission = (int)sales * 0.1;
-        else if (sales > 500)
-            commission = (int)sales * 0.15;
-        else commission = 0;
-    }
-    
-    public double getTotalSales(){
+        
+    public double calculateCommission() {
+        if (sales >= 150 && sales <= 300){
+            commission =(int) sales * 0.05;}
+        else if (sales >= 301 && sales <= 500){
+            commission = (int)sales * 0.1;}
+        else if (sales > 500){
+            commission = (int)sales * 0.15;}
+        else {
+            commission = 0;}
+        
         return commission;
     }
-    
+        
     public void displaySalary(){
         System.out.println("-----Staff-------");
         System.out.println("Staff Name : " + name);
         System.out.println("StaffID : " + staffID);
-        System.out.println("Hours Work : " + totalHourWorks);
-        System.out.println("Total sales : " + "RM" + commission * sales);
-        System.out.println("Total Salary : " + "RM" +salary);
+        System.out.println("Hours Work : " + hourWorks);
+        System.out.println("Total sales : " + "RM" + sales);
+        System.out.println("Total Salary : " + "RM" + calculateSalary());
         System.out.println("------------------");
         
     }
