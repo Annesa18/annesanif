168b6c282e5d41f9997b58d420a853366e336eb9
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-04-08 12:03:39 +0800

    Update CubesAndSquares.java

--- Exercise2/CubesAndSquares.java
+++ Exercise2/CubesAndSquares.java
@@ -1,16 +1,15 @@
-public class CubesAndSquares{ // class name
-public static void main (String []args){ // main method
-		int num =0; //declare num is int and variable is 0.
-		int square =0; //declare square is int and variable is 0.
-		int cube =0; //declare cube is int and variable is 0.
+public class CubesAndSquares{ 
+	public static void main (String []args){ // main method
+		int num =0; 
+		int square =0;
+		int cube =0; 
 		
 		
-		System.out.printf("Number\tSquare\tCube\n");  //display number,square, cube
-      for(int i=0; i<=10; i++) //using for looping number
-		{
+		System.out.printf("Number\tSquare\tCube\n");  
+      		for(int i=0; i<=10; i++) {
 			square= num*num;  // calculation for square
 			cube= num*square; // calculation for cube
-			System.out.printf("%4d%8d%9d\n", num, square, cube); //display number,square,cube
+			System.out.printf("%4d%8d%9d\n", num, square, cube); 
 			num++;
 		}
 	}
