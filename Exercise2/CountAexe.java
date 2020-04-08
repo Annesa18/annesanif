965349cf7b7fd216c1775cfa01c8f19493b6aca2
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-04-08 12:02:03 +0800

    Update CountAexe.java

--- Exercise2/CountAexe.java
+++ Exercise2/CountAexe.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
 public class CountAexe{
-public static void main (String []args){
+    public static void main (String []args){
 
-Scanner in = new Scanner (System.in);
-String str;
-int count =0;
+    Scanner in = new Scanner (System.in);
+    String str;
+    int count =0;
 
-System.out.println("Enter the string:");
-str= in.nextLine();
+    System.out.println("Enter the string:");
+    str= in.nextLine();
 
-for (int i=0; i<str.length();i++){
-    if (str.charAt(i)=='A'){
-    count++;
+    for (int i=0; i<str.length();i++){
+        if (str.charAt(i)=='A'){
+        count++;
+        }
     }
-   }
-System.out.println("Count of A is found:" + count);
-}
+    System.out.println("Count of A is found:" + count);
+    }
 }    
-
-
-
-
-
-
-
