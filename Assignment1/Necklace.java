859d7f25d26f4f472383c5b04a8deb6352eed340
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-05-31 11:25:27 +0800

    Update Necklace.java

--- Assignment1/Necklace.java
+++ Assignment1/Necklace.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Jewellery;
 
-/**
- *
- * @author nurul annesa
- */
 public class Necklace extends Jewellery {
 
     private int price;
