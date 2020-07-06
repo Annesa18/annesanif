ac395b80ef4c813baa328267b43e353f7dcce131
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-07-06 11:30:33 +0800

    Add files via upload

---
+++ Jewellery.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author nurul annesa
+ */
+public class Jewellery {
+    String name,ic,mPhone,address,type;
+    
+    public Jewellery(String name,String ic,String m,String add,String type){
+        this.name = name;
+        this.ic = ic;
+        this.type = type;
+        mPhone = m;
+        address = add;
+    }
+}
