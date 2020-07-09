cdb3efee07e5b28b9d29346dafe293cdbc147396
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-07-09 15:14:34 +0800

    Update Cat.java

--- quiz2/Cat.java
+++ quiz2/Cat.java
@@ -1,23 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quiz;
 
-/**
- *
- * @author nurul annesa
- */
 public class Cat extends Animals {
 
-    /*String name;
-    int age;
-
-    Cat(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }*/
     String name;
 
     Cat(String kurre) {
