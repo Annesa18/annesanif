f887f3be45f08fc9345efb9d8f1c5fc1ad86d77b
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-07-09 15:15:04 +0800

    Update Dog.java

--- quiz2/Dog.java
+++ quiz2/Dog.java
@@ -1,23 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quiz;
 
-/**
- *
- * @author nurul annesa
- */
 public class Dog extends Animals {
 
-    /*String name;
-    int age;
-
-    Dog(String name,int age) {
-        this.name = name;
-        this.age = age;
-    }*/
+   
     String name;
 
     Dog(String vilma) {
