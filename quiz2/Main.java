750f874a6a537286b00b81a47d9e7f6c7b37333f
Annesa18 <61263813+Annesa18@users.noreply.github.com>
2020-07-09 15:15:35 +0800

    Update Main.java

--- quiz2/Main.java
+++ quiz2/Main.java
@@ -1,36 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quiz;
 
-/**
- *
- * @author nurul annesa
- */
-public class Main {
+c class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
 
-        /*Animals[] animals;
-        int i;
-
-        animals = new Animals[3];
-
-        animals[0] = new Cat("Kurre");
-        animals[1] = new Dog("Vilma");
-        animals[2] = new Cat("Bamse");
-
-        i = 0;
-        while (i < animals.length) {
-            animals[i].introduceYourself();
-            i = i + 1;
-        }*/
         Animals kurre = new Cat("Kurre");
         Animals vilma = new Dog("Vilma");
 
